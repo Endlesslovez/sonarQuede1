243365f1ea5b4b7b9f8919d61bef078bb7d764d1
minhminh <nguyenxuanminh10@gmail.com>
2021-05-09 12:38:12 +0700

    sdsd

---
+++ src/main/java/com/example/demo/DemoGitHub1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitHub1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitHub1Application.class, args);
+	}
+
+}
